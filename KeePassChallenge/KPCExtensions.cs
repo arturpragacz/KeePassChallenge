454f3d5340284112f1f4fa5d837943ef1765cfba
Artur Pragacz <artur@pragacz.com>
2022-06-12 07:21:10 +0200

    Add GetSeed extension method to KdfEngines.

---
+++ KeePassChallenge/KPCExtensions.cs
@@ -0,0 +1,26 @@
+﻿using KeePassLib.Cryptography.KeyDerivation;
+
+namespace KeePassChallenge {
+	internal static class Extensions {
+		public static byte[] GetSeed(this KdfEngine kdf, KdfParameters p) {
+			var getSeed = new GetSeedClass { Par = p };
+			return getSeed.Do((dynamic)kdf);
+		}
+
+		private class GetSeedClass {
+			public KdfParameters Par { get; set; }
+
+			public byte[] Do(KdfEngine kdf) {
+				return null;
+			}
+
+			public byte[] Do(Argon2Kdf kdf) {
+				return Par.GetByteArray(Argon2Kdf.ParamSalt);
+			}
+
+			public byte[] Do(AesKdf kdf) {
+				return Par.GetByteArray(AesKdf.ParamSeed);
+			}
+		}
+	}
+}
